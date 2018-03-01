c78e335eb3782c2d6a0f24b9072fe5113fd1f048
Ray Linden <raylinden01@gmail.com>
2018-03-01 13:51:04 -0500

    Added RF rx & tx drivers source from TI resource explorer.

---
+++ RF/rfPacketRx_CC1350_LAUNCHXL_nortos_ccs/Debug/subdir_vars.mk
@@ -0,0 +1,53 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../CC1350_LAUNCHXL_NoRTOS.cmd 
+
+C_SRCS += \
+../CC1350_LAUNCHXL.c \
+../RFQueue.c \
+../ccfg.c \
+../main_nortos.c \
+../rfPacketRx.c 
+
+C_DEPS += \
+./CC1350_LAUNCHXL.d \
+./RFQueue.d \
+./ccfg.d \
+./main_nortos.d \
+./rfPacketRx.d 
+
+OBJS += \
+./CC1350_LAUNCHXL.obj \
+./RFQueue.obj \
+./ccfg.obj \
+./main_nortos.obj \
+./rfPacketRx.obj 
+
+OBJS__QUOTED += \
+"CC1350_LAUNCHXL.obj" \
+"RFQueue.obj" \
+"ccfg.obj" \
+"main_nortos.obj" \
+"rfPacketRx.obj" 
+
+C_DEPS__QUOTED += \
+"CC1350_LAUNCHXL.d" \
+"RFQueue.d" \
+"ccfg.d" \
+"main_nortos.d" \
+"rfPacketRx.d" 
+
+C_SRCS__QUOTED += \
+"../CC1350_LAUNCHXL.c" \
+"../RFQueue.c" \
+"../ccfg.c" \
+"../main_nortos.c" \
+"../rfPacketRx.c" 
+
+
